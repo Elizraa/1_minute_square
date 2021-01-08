f741f815ba61625934154e6503b2530a04ebf135
Elizraa <56952777+Elizraa@users.noreply.github.com>
2021-01-08 11:32:47 +0700

    camera control

---
+++ SquareGame/Assets/Asset/PlayerControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D)) moveLeft();
+        else if(Input.GetKeyDown(KeyCode.A)) moveRight();
+    }
+
+    void moveLeft()
+    {
+        transform.position = new Vector3(transform.position.x+2.35f, transform.position.y, transform.position.z);
+    }
+    void moveRight()
+    {
+        transform.position = new Vector3(transform.position.x-2.35f, transform.position.y, transform.position.z);
+    }
+}
